462b29dda9f8fb326a94b2dfb3afdeaa561e0541
nick22white <nicholas22white@icloud.com>
2021-04-10 17:09:52 -0700

    Authentication Login

--- EgyptExcavation/Startup.cs
+++ EgyptExcavation/Startup.cs
@@ -43,7 +43,8 @@ namespace EgyptExcavation
                  .AddDefaultTokenProviders();
             services.AddRazorPages();
             services.AddMvc();
-               
+
+           
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
