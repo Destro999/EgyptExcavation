1a5d12fcc28feb18ee7f317fd91394096e60d34d
Destro999 <70977094+Destro999@users.noreply.github.com>
2021-04-12 08:09:46 -0600

    Changes for the upload file

--- EgyptExcavation/wwwroot/js/site.js
+++ EgyptExcavation/wwwroot/js/site.js
@@ -2,6 +2,8 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+
 var slideIndex = 1;
 showDivs(slideIndex);
 
@@ -21,14 +23,3 @@ function showDivs(n) {
 }
 
 
-//function highlight {
-//    var header = document.getElementById("myDIV");
-//    var btns = header.getElementsByClassName("btna");
-//    for (var i = 0; i < btns.length; i++) {
-//        btns[i].addEventListener("click", function () {
-//            var current = document.getElementsByClassName("Current");
-//            current[0].className = current[0].className.replace(" Current", "");
-//            this.className += " Current";
-//        });
-//    }
-//}
